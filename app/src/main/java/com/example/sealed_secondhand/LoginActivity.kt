83fa91f1d5e13a03da91a798afc559d6bee290fd
keon22han <keonheehan@hansung.ac.kr>
2023-11-05 01:05:09 +0900

    Implement Sign Up Functionality and Resolve Bugs
    Implement Sign Up Functionality and Resolve Bugs

--- app/src/main/java/com/example/sealed_secondhand/LoginActivity.kt
+++ app/src/main/java/com/example/sealed_secondhand/LoginActivity.kt
@@ -1,29 +1,26 @@
 package com.example.sealed_secondhand
 
-import android.animation.AnimatorInflater
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.transition.Scene
-import androidx.transition.ChangeBounds
 import androidx.transition.Fade
 import androidx.transition.Transition
 import androidx.transition.TransitionInflater
 import androidx.transition.TransitionManager
 import com.example.sealed_secondhand.db.FirebaseAuth
-import com.google.android.material.transition.MaterialContainerTransform.FadeMode
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var firebaseauth: FirebaseAuth
     private lateinit var scene1 : Scene
     private lateinit var loginButton : Button
+    private lateinit var signupButton : Button
+    private lateinit var idEditText : EditText
+    private lateinit var pwEditText : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -37,21 +34,44 @@ class LoginActivity : AppCompatActivity() {
             Thread.sleep(100)
             runOnUiThread {
                 TransitionManager.go(scene1, transition)
+                idEditText = findViewById(R.id.idEditText)
+                pwEditText = findViewById(R.id.pwEditText)
                 loginButton = findViewById(R.id.loginButton)
                 loginButton.setOnClickListener {
-                    FirebaseAuth.doLogin(findViewById<EditText>(R.id.idEditText).text.toString(), findViewById<EditText>(R.id.pwEditText).text.toString())
-                        .addOnCompleteListener(this) {
-                        if (it.isSuccessful) {
-                            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
-                            val intent = Intent(applicationContext, MainActivity::class.java)
-                            startActivity(intent)
-                            finish()
-                        }
-                        else {
-                            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
-                        }
+                    if (idEditText.text.toString() != "" && pwEditText.text.toString() != "") {
+                        FirebaseAuth.doLogin(idEditText.text.toString(), pwEditText.text.toString())
+                            .addOnCompleteListener(this) {
+                                if (it.isSuccessful) {
+                                    Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+                                    val intent = Intent(applicationContext, MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                } else {
+                                    Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                    } else {
+                        Toast.makeText(this, "ID 및 PASSWORD를 입력해주세요.", Toast.LENGTH_SHORT).show()
                     }
                 }
+                signupButton = findViewById(R.id.signupButton)
+                signupButton.setOnClickListener {
+                    if (idEditText.text.toString() != "" && pwEditText.text.toString() != "") {
+                        FirebaseAuth.doSignUp(idEditText.text.toString(), pwEditText.text.toString())
+                            .addOnCompleteListener(this) {
+                                if (it.isSuccessful) {
+                                    Toast.makeText(this, "SignUp Success, 로그인을 진행해 주세요.", Toast.LENGTH_SHORT).show()
+                                    idEditText.setText("")
+                                    pwEditText.setText("")
+                                } else {
+                                    Toast.makeText(this, "SignUp Failed", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                    } else {
+                        Toast.makeText(this, "ID 및 PASSWORD를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
             }
         }
         thread.start()
